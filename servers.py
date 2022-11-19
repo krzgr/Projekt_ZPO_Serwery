5094cad1838019af8da5e1f2c64b7871cd26944e
ziembamilosz <91010600+ziembamilosz@users.noreply.github.com>
2022-11-19 18:35:29 +0100

    unittests

--- servers.py
+++ servers.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from typing import Optional, Tuple, List
+from typing import Optional, List
 from abc import ABC, abstractmethod
 from copy import deepcopy
 import re
 
 
 class Product:
-    def __init__(self, name : str = "test123" , price : float = 0.0) -> None:
+    def __init__(self, name: str = "test123", price: float = 0.0) -> None:
         self.set_name(name)
         self.set_price(price)
 
-    def set_name(self, name : str):
+    def set_name(self, name: str):
         if isinstance(name, str) and re.fullmatch("^[a-zA-Z]+\d+$", name):
             self.name = name
         else:
             raise ValueError("Nazwa produktu musi być w postaci <ciąg_liter><ciąg cyfr>!")
     
-    def set_price(self, price : float):
+    def set_price(self, price: float):
         if isinstance(price, (float, int)) and price >= 0:
             self.price = float(price)
         else:
@@ -42,30 +42,31 @@ class Product:
 class TooManyProductsFoundError(Exception):
     pass
 
+
 class Server(ABC):
     n_max_returned_entries = 3
 
     @abstractmethod
-    def __init__(self, products : List[Product] = []) -> None:
+    def __init__(self, products: List[Product] = []) -> None:
         pass
     
     @abstractmethod
-    def add_product(self, product : Product) -> None:
+    def add_product(self, product: Product) -> None:
         pass
 
     @abstractmethod
-    def get_entries(self, n_letters : int) -> List[Product]:
+    def get_entries(self, n_letters: int) -> List[Product]:
         pass
 
 
 class ListServer(Server):
-    def __init__(self, products : List[Product] = []) -> None:
+    def __init__(self, products: List[Product] = []) -> None:
         self.products = deepcopy(products)
 
-    def add_product(self, product : Product) -> None:
+    def add_product(self, product: Product) -> None:
         self.products.append(product)
     
-    def get_entries(self, n_letters : int = 1) -> List[Product]:
+    def get_entries(self, n_letters: int = 1) -> List[Product]:
         if isinstance(n_letters, int) and n_letters > 0:
             pattern = "^[a-zA-Z]{" + str(n_letters) + "}\d{2,3}$"
             result = [x for x in self.products if re.fullmatch(pattern, x.get_name())]
@@ -74,14 +75,15 @@ class ListServer(Server):
             result.sort(key=lambda a: a.get_price())
             return result
 
-class MapServer(Server):
-    def __init__(self, products : List[Product] = []) -> None:
-        self.products = {x.get_name() : x for x in products}
 
-    def add_product(self, product : Product) -> None:
+class MapServer(Server):
+    def __init__(self, products: List[Product] = []) -> None:
+        self.products = {x.get_name(): x for x in products}
+
+    def add_product(self, product: Product) -> None:
         self.products[product.get_name()] = product
     
-    def get_entries(self, n_letters : int) -> List[Product]:
+    def get_entries(self, n_letters: int) -> List[Product]:
         if isinstance(n_letters, int) and n_letters > 0:
             pattern = "^[a-zA-Z]{" + str(n_letters) + "}\d{2,3}$"
             result = [self.products[key] for key in self.products.keys() if re.fullmatch(pattern, key)]
@@ -92,7 +94,7 @@ class MapServer(Server):
 
 
 class Client:
-    def __init__(self, server : Server) -> None:
+    def __init__(self, server: Server) -> None:
         self.server = server
         
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
@@ -100,7 +102,6 @@ class Client:
             tmp = self.server.get_entries(n_letters)
             if len(tmp) == 0:
                 return None
-
             result = 0
             for prod in tmp:
                 result += prod.get_price()
--- servers__tests.py
+++ servers__tests.py
@@ -1,28 +1,133 @@
 import unittest
-from collections import Counter
 
-from servers import ListServer, Product, Client, MapServer
+from servers import ListServer, Product, Client, MapServer, TooManyProductsFoundError
 
 server_types = (ListServer, MapServer)
 
 
 class ServerTest(unittest.TestCase):
 
-    def test_get_entries_returns_proper_entries(self):
+    def test_get_entries_returns_proper_entries1(self):
         products = [Product('P12', 1), Product('PP234', 2), Product('PP235', 1)]
         for server_type in server_types:
             server = server_type(products)
             entries = server.get_entries(2)
-            self.assertEqual(Counter([products[2], products[1]]), Counter(entries))
+            self.assertEqual([products[2], products[1]], entries)
+
+    def test_get_entries_returns_proper_entries2(self):
+        products = [Product('PPP12', 5), Product('PPP234', 2), Product('PP235', 1), Product('PPP112', 1)]
+        for server_type in server_types:
+            server = server_type(products)
+            entries = server.get_entries(3)
+            self.assertEqual([products[3], products[1], products[0]], entries)
+
+    def test_get_entries_returns_proper_entries3(self):
+        products = []
+        for server_type in server_types:
+            server = server_type(products)
+            entries = server.get_entries(3)
+            self.assertEqual([], entries)
+
+
+class RaisingExceptionsTest(unittest.TestCase):
+
+    def test_if_product_raised_proper_exception1(self):
+        with self.assertRaises(ValueError):
+            Product('111aaa', 15.0)
+
+    def test_if_product_raised_proper_exception2(self):
+        with self.assertRaises(ValueError):
+            Product('111', 15.0)
+
+    def test_if_each_server_raised_proper_exception1(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.2),
+                    Product('PPP146', 3.9), Product('PPP928', 6.1), Product('PPP719', 6.2)]
+        for server_type in server_types:
+            server = server_type(products)
+            with self.assertRaises(TooManyProductsFoundError):
+                server.get_entries(3)
+
+    def test_if_each_server_raised_proper_exception2(self):
+        products = [Product('P12', 1.5), Product('P235', 1.2),
+                    Product('P928', 6.1), Product('P719', 6.2)]
+        for server_type in server_types:
+            server = server_type(products)
+            with self.assertRaises(TooManyProductsFoundError):
+                server.get_entries(1)
+
+    def test_if_each_server_raised_proper_exception3(self):
+        products = [Product('P12', 1.5), Product('P34', 2.3), Product('PPP235', 1.2),
+                    Product('PPP146', 3.9), Product('PPP928', 6.1), Product('PP719', 6.2),
+                    Product('P10', 1), Product('P24', 3), Product('PPP635', 2),
+                    Product('PP000', 6), Product('PP763', 18), Product('PP173', 2.1)]
+        for server_type in server_types:
+            server = server_type(products)
+            with self.assertRaises(TooManyProductsFoundError):
+                server.get_entries(3)
+            with self.assertRaises(TooManyProductsFoundError):
+                server.get_entries(1)
+
+
+class ListServerTestSortResult(unittest.TestCase):
+
+    def test_if_entries_are_sorted_properly1(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.2)]
+        server = ListServer(products)
+        entries = server.get_entries(3)
+        self.assertEqual([entry.get_price() for entry in entries], [1.2, 1.5, 2.3])
+
+    def test_if_entries_are_sorted_properly2(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.2), Product('PP111', 1.8)]
+        server = ListServer(products)
+        entries = server.get_entries(3)
+        self.assertEqual([entry.get_price() for entry in entries], [1.2, 1.5, 2.3])
+
+    def test_if_entries_are_sorted_properly3(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.5), Product('PP111', 1.8)]
+        server = ListServer(products)
+        entries = server.get_entries(3)
+        self.assertEqual([entry.get_price() for entry in entries], [1.5, 1.5, 2.3])
 
 
 class ClientTest(unittest.TestCase):
-    def test_total_price_for_normal_execution(self):
+
+    def test_total_price_for_normal_execution1(self):
         products = [Product('PP234', 2), Product('PP235', 3)]
         for server_type in server_types:
             server = server_type(products)
             client = Client(server)
             self.assertEqual(5, client.get_total_price(2))
+
+    def test_total_price_for_normal_execution2(self):
+        products = [Product('PPP234', 2), Product('PP235', 3), Product('P123', 4),
+                    Product('PP923', 1), Product('P999', 15), Product('PPP128', 8)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(19, client.get_total_price(1))
+            self.assertEqual(4, client.get_total_price(2))
+            self.assertEqual(10, client.get_total_price(3))
+
+    def test_total_price_sensitive_case1(self):
+        products = []
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(None, client.get_total_price(2))
+
+    def test_total_price_sensitive_case2(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.5), Product('PPP111', 1.8)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(None, client.get_total_price(2))
+
+    def test_total_price_sensitive_case3(self):
+        products = [Product('PPP12', 1.5), Product('PPP34', 2.3), Product('PPP235', 1.5), Product('PPP111', 1.8)]
+        for server_type in server_types:
+            server = server_type(products)
+            client = Client(server)
+            self.assertEqual(None, client.get_total_price(3))
 
 
 if __name__ == '__main__':
